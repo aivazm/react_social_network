51f9573dc321ae9cc8a90087760bc09cb5dbdff3
Aivaz Muldamuratov <aivazm2@gmail.com>
2020-03-13 22:15:34 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+    <header className={'header'}>
+        <img src='https://cdn.shopify.com/shopifycloud/hatchful-web/assets/6fcc76cfd1c59f44d43a485167fb3139.png' />
+    </header>);
+};
+
+export default Header;
