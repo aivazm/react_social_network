c6fd868f28f3d20f6a1131a9bd4e4b5f7170f000
Aivaz Muldamuratov <aivazm2@gmail.com>
2020-03-15 14:25:20 +0300

    Replaced <a> by <NavLink>

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import classes from './Navbar.module.css'
+import {NavLink} from "react-router-dom"
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
             <div className={classes.item}>
-                <a href={"/profile"}>Profile</a>
+                <NavLink to={"/profile"} activeClassName={classes.activeLink}>Profile</NavLink>
             </div>
             <div className={classes.item}>
-                <a href={"/dialogs"}>Messages</a>
+                <NavLink to={"/dialogs"} activeClassName={classes.activeLink}>Messages</NavLink>
             </div>
             <div className={classes.item}>
-                <a href={"/news"}>News</a>
+                <NavLink to={"/news"} activeClassName={classes.activeLink}>News</NavLink>
             </div>
         </nav>
     );
