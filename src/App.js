82ab23cf323965b2b3d43ae0a4a619b8bb9064bf
Aivaz Muldamuratov <aivazm2@gmail.com>
2020-03-15 14:09:49 +0300

    Add Route components on main page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import './App.css';
 import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
 import Dialogs from "./components/dialogs/Dialogs";
+import Profile from "./components/profile/Profile";
+import {BrowserRouter, Route} from "react-router-dom"
+import News from "./components/news/News";
 
 const App = () => {
     return (
+        <BrowserRouter>
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
             <div className='app-wrapper-content'>
-                <Dialogs/>
+                <Route path ='/dialogs' component={Dialogs}/>
+                <Route path ='/profile' component={Profile}/>
+                <Route path ='/news' component={News}/>
             </div>
         </div>
+        </BrowserRouter>
     )
 };
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import './Navbar.module.css';
 import classes from './Navbar.module.css'
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
             <div className={classes.item}>
-                Profile
+                <a href={"/profile"}>Profile</a>
             </div>
             <div className={classes.item}>
-                Messages
+                <a href={"/dialogs"}>Messages</a>
             </div>
             <div className={classes.item}>
-                News
-            </div>
-            <div className={classes.item}>
-                Music
-            </div>
-            <p/>
-            <div className={`${classes.item} ${classes.active}`}>
-                 Settings
+                <a href={"/news"}>News</a>
             </div>
         </nav>
     );
