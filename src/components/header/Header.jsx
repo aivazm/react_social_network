b9bd679e7718b2bca2f61f3dc7e518936177dd1e
Aivaz Muldamuratov <aivazm2@gmail.com>
2020-05-06 23:26:23 +0300

    Some fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,11 +6,8 @@ import logo from '../../assets/images/logo.png'
 const Header = () => {
     return (
         <header className={classes.header}>
-            <div className={classes.layer1}>
+            <div>
                 <img src={logo} alt={'some alt'}/>
-            </div>
-            <div className={classes.layer2}>
-                <h1>Зулейха видит барана</h1>
             </div>
         </header>
     );
